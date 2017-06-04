e23c0fd42a962b073303167858131fb3df29da5f
kevin Cleathero <kengc@hotmail.com>
2017-06-04 14:42:10 -0700

    added dictionary to player init

--- SnakesLadders/Player.m
+++ SnakesLadders/Player.m
@@ -15,6 +15,19 @@
     self = [super init];
     if (self) {
         _currentSquare = 0;
+        _gameLogic = [[NSDictionary alloc] initWithObjectsAndKeys:
+    [NSNumber numberWithInt:4],[NSNumber numberWithInt:14],
+    [NSNumber numberWithInt:9],[NSNumber numberWithInt:31],
+    [NSNumber numberWithInt:17], [NSNumber numberWithInt:7],
+    [NSNumber numberWithInt:20],[NSNumber numberWithInt:38],
+    [NSNumber numberWithInt:28],[NSNumber numberWithInt:84],
+    [NSNumber numberWithInt:40],[NSNumber numberWithInt:59],
+    [NSNumber numberWithInt:51],[NSNumber numberWithInt:67],
+    [NSNumber numberWithInt:63],[NSNumber numberWithInt:81],
+    [NSNumber numberWithInt:64],[NSNumber numberWithInt:60],
+    [NSNumber numberWithInt:89],[NSNumber numberWithInt:26],
+    [NSNumber numberWithInt:95],[NSNumber numberWithInt:75],
+    [NSNumber numberWithInt:99],[NSNumber numberWithInt:78], nil];
     }
     return self;
 }
